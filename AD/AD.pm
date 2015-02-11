a6e7fa5e493cded29ab3899e0d9a627dcf27b824
fwiffo <borgified@gmail.com>
2015-02-11 13:30:19 -0800

    adding AD module to disable AD accounts

---
+++ AD/AD.pm
@@ -0,0 +1,58 @@
+package AD;
+
+use warnings;
+use strict;
+use Exporter;
+use Net::OpenSSH;
+
+use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
+
+$VERSION     = 1.00;
+@ISA         = qw(Exporter);
+@EXPORT      = ();
+@EXPORT_OK   = qw(deactivate);
+%EXPORT_TAGS = ( all => [qw(&deactivate)],);
+
+my %config = do '/secret/AD.conf';
+
+sub deactivate{
+	my $email = shift;
+
+	my $login_error;
+	my ($stdout,$stderr,$exit);
+
+	my $ssh = Net::OpenSSH->new($config{nagios_server},
+		user => $config{user},
+		passwd => $config{passwd},
+		ctl_dir => '/home/apache',
+		master_opts => [-o => "StrictHostKeyChecking=no",
+			-o => "UserKnownHostsFile=/home/apache/.ssh/known_hosts",
+		],
+	);
+
+	my $output;
+
+	if($ssh->error){
+		$output = "cant ssh to $config{nagios_server}: ". $ssh->error;
+		return $output;
+	}
+
+	my $capture = $ssh->capture({timeout => 20},"/usr/lib/nagios/plugins/check_nrpe -H $config{AD_server} -c disable_AD_user -a $email");
+
+	if($capture =~ /Management object not found/){
+		$output = "user not found";
+	}elsif($capture =~ /No output available from command/){
+		#if theres no output, it means it ran successfully
+		$output = "deactivated";
+	}else{
+		#not sure what happened but ill show you the capture for debugging
+		$output = $capture;
+	}
+
+
+	return $output;
+
+}
+
+
+1;
---
+++ AD/testAD.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+
+use AD;
+
+print AD::deactivate('thisperson@company.com');
